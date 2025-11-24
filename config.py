0190590ea6c6ef1dbd2158bb79d4b1541a110513
drekain <drekainsmith@gmail.com>
2025-11-24 15:48:25 +0530

    Change default values for API_ID and OWNER_ID
    Updated API_ID and OWNER_ID to default to None if not valid.

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# Authored By Certified Coders — v1.2 (2025-11-14)
 import os
 from dotenv import load_dotenv
 
@@ -10,5 +9,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = os.getenv("OWNER_ID")
 MONGO_URI = os.getenv("MONGO_URI")
 
-API_ID = int(API_ID) if API_ID and API_ID.isdigit() else 27798659
-OWNER_ID = int(OWNER_ID) if OWNER_ID and OWNER_ID.isdigit() else 7019293589
+API_ID = int(API_ID) if API_ID and API_ID.isdigit() else None
+OWNER_ID = int(OWNER_ID) if OWNER_ID and OWNER_ID.isdigit() else None
